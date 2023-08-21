28505cee320884f32c8c57260099cd6bf6da21cb
Marcos Pontes <socrampontes@gmail.com>
2023-08-21 20:27:28 -0300

    autentidação II template

--- src/dtos/user/getUsers.dto.ts
+++ src/dtos/user/getUsers.dto.ts
@@ -3,11 +3,13 @@ import { UserModel } from "../../models/User"
 
 export interface GetUsersInputDTO {
   q: string
+  token:string
 }
 
 // UserModel é a estrutura de User que será devolvida para o Front (sem password)
 export type GetUsersOutputDTO = UserModel[]
 
 export const GetUsersSchema = z.object({
-  q: z.string().min(1).optional()
+  q: z.string().min(1).optional(),
+  token: z.string().min(2).optional()
 }).transform(data => data as GetUsersInputDTO)
